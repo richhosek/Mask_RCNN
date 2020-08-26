7268903ef100d1903325aebb39d10d0d600a352e
Rich Hosek <richhosek@gmail.com>
2020-08-26 15:04:35 -0500

    Epochs back to 30

--- vehicle.py
+++ vehicle.py
@@ -215,8 +215,8 @@ def train(model):
     print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                # epochs=30,
-                epochs=5,
+                epochs=30,
+                # epochs=5,
                 layers='heads')
 
 
